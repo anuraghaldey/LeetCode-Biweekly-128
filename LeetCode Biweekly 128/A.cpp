510e71d3bb30433b0283006e004251bc9a938153
Anurag Haldey <aanurag2003haldey@gmail.com>
2024-04-14 00:55:08 +0530

    Update A.cpp

--- LeetCode Biweekly 128/A.cpp
+++ LeetCode Biweekly 128/A.cpp
@@ -58,10 +58,10 @@ iterator
 class Solution {
 public:
     int scoreOfString(string s) {
-         int score = 0;
-    for (int i = 1; i < s.length(); ++i) {
-        score += abs(s[i] - s[i - 1]);
-    }
-    return score;
+            int score = 0;
+	    for (int i = 1; i < s.length(); ++i) {
+		score += abs(s[i] - s[i - 1]);
+	    }
+	    return score;
     }
 };
